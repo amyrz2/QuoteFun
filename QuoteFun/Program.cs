425551ec92237b19f347eab4b65703228ff52631
amyzhang <amyzhang@AMYZHANGB686>
2023-01-25 11:50:22 -0700

    Add project files.

---
+++ QuoteFun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuoteFun
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string quote = "";
+
+            Console.WriteLine("Welcome to the Quote Analyzer!");
+            Console.WriteLine("Please enter a quote");
+            quote = Console.ReadLine();
+
+        }
+    }
+}
